858509662cef43b33d4fd6dd37fdaa010faff096
thokozani tsana <tsanathokozani@gmail.com>
2024-06-13 12:53:27 +0200

    Changes

--- polls/models.py
+++ polls/models.py
@@ -2,16 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
+    """A class representing questions for voters."""
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
+        """A method for questions, and it returns the questions-text."""
         return self.question_text
 
 class Choice(models.Model):
+   """A Class to represent the user's choice of answers to the questions."""
+
    question = models.ForeignKey(Question, on_delete=models.CASCADE)
    choice_text = models.CharField(max_length=200)
    votes = models.IntegerField(default=0)
 
    def __str__(self):
+        """A method of class choices which returns the choice-text."""
         return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -6,19 +6,27 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):
+    """A index function that takes in the request and returns render request and html file and context."""
+
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, "polls/poll.html", context)
 
 def detail(request, question_id):
+    """detail function that takes the request and questions id and returns render request, html file, and question."""
+
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
+    """result function that takes request, question id returns render request,html and questions."""
+
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
+    """vote function that takes in request and question id with the use of try-except if  exception occurs the won't be issues."""
+    
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(
